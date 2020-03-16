599a11e6681d9ae44bd8ee23ac88fbc6c9d30849
rheimon1 <wallaceramonns.dev@usp.br>
2020-03-16 18:26:46 -0300

    Initial commit

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import os
 import requests
-
-from telegram import Update, Bot
-from telegram.ext import CommandHandler, Dispatcher
+from telegram import Bot, Update
+from telegram.ext import Dispatcher, CommandHandler
 
 
 def brazil(bot, update: Update, **optional_args):
-
-     # Dados da requisição feita na API do coronaanalytic
+    # Dados da requisição feita na API do coronaanalytic
     dataBrazil = requests.get("https://api.coronaanalytic.com/brazil").json()
 
     values = dataBrazil["values"]
 
-    deaths = 0
-    cases = 0
-    suspects = 0
+    deaths, cases, suspects = 0, 0, 0
     date = str(dataBrazil["date"])
     time = str(dataBrazil["time"])
 
@@ -29,13 +25,10 @@ def brazil(bot, update: Update, **optional_args):
 
     response_message = "Informações gerais sobre o corona vírus (COVID-19) no Brasil\n\n - Casos: {}; \n - Suspeitas: {}; \n - Mortes: {}. \n\n Última atualização: {} às {}. \n\n Todas as informações são atualizadas a cada hora com a fonte de dados fornecida pelo governo do Brasil: https://saude.gov.br"
 
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text=response_message.format(cases, suspects, deaths, date, time)
-    )
+    update.message.reply_text(response_message.format(cases, suspects, deaths, date, time), quote=False)
+
 
 def world(bot, update: Update, **optional_args):
-
     req = requests.get("https://api.coronaanalytic.com/").json()
 
     dataWorld = req["world"]
@@ -59,10 +52,8 @@ def world(bot, update: Update, **optional_args):
 
     response_message = "Informações gerais sobre o corona vírus (COVID-19) no mundo\n\n - Casos: {} \n - Suspeitas: {} \n - Mortes: {}. \n\n Última atualização: {} às {}. \n\n Todas as informações são atualizadas a cada hora com a fonte de dados fornecida pelo governo do Brasil: https://saude.gov.br"
 
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text=response_message.format(cases, suspects, deaths, date, time)
-    )
+    update.message.reply_text(response_message.format(cases, suspects, deaths, date, time), quote=False)
+
 
 def sao_paulo(bot, update: Update, **optional_args):
     dataSaoPaulo = requests.get("https://api.coronaanalytic.com/brazil/35").json()
@@ -72,21 +63,23 @@ def sao_paulo(bot, update: Update, **optional_args):
     deaths = dataSaoPaulo["deaths"]
 
     response_message = "Informações gerais sobre o corona vírus (COVID-19) em São Paulo\n\n - Casos: {}; \n - Suspeitas: {}; \n - Mortes: {}. \n\n Todas as informações são atualizadas a cada hora com a fonte de dados fornecida pelo governo do Brasil: https://saude.gov.br"
-    
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text=response_message.format(cases, suspects, deaths)
-    )
+
+    update.message.reply_text(response_message.format(cases, suspects, deaths), quote=False)
+
+
+def start(bot, update: Update, **optional_args):
+    update.message.reply_text('OIII', quote=False)
+
 
 def webhook(request):
-    bot = Bot(token=os.environ["TELEGRAM_TOKEN"])
+    bot = Bot(token=os.environ['TELEGRAM_TOKEN'])
     dispatcher = Dispatcher(bot, None, 0)
-    dispatcher.add_handler(CommandHandler("coronaBrasil", brazil))
-    dispatcher.add_handler(CommandHandler("coronaMundo", world))
-    dispatcher.add_handler(CommandHandler("coronaSP", sao_paulo))
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('coronaMundo', world))
+    dispatcher.add_handler(CommandHandler('coronaBrasil', brazil))
+    dispatcher.add_handler(CommandHandler('coronaSP', sao_paulo))
 
-    if request.method == "POST":
+    if request.method == 'POST':
         update = Update.de_json(request.get_json(force=True), bot)
         dispatcher.process_update(update)
-
-    return "ok"
+    return 'ok'
