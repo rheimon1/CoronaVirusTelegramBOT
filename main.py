e3ea73f84d2a4f02f54fbdefba59dc10e576126b
Wallace Ramon Nogueira Soares <49095811+rheimon1@users.noreply.github.com>
2020-03-17 13:43:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ def world(bot, update: Update):
 
     deaths = 0
     cases = 0
-    suspects = 0
     date = str(dataWorld["date"])
     time = str(dataWorld["time"])
 
@@ -47,12 +46,10 @@ def world(bot, update: Update):
                 deaths += value
             if key == 'cases':
                 cases += value
-            if key == 'suspects':
-                suspects += value
 
-    response_message = "Informações gerais sobre o corona vírus (COVID-19) no mundo\n\n - Casos: {} \n - Suspeitas: {} \n - Mortes: {}. \n\n Última atualização: {} às {}. \n\n Todas as informações são atualizadas a cada hora com a fonte de dados fornecida pelo governo do Brasil: https://saude.gov.br"
+    response_message = "Informações gerais sobre o corona vírus (COVID-19) no mundo\n\n - Casos: {} \n - Mortes: {}. \n\n Última atualização: {} às {}. \n\n Todas as informações são atualizadas a cada hora com a fonte de dados fornecida pelo governo do Brasil: https://saude.gov.br"
 
-    update.message.reply_text(response_message.format(cases, suspects, deaths, date, time), quote=False)
+    update.message.reply_text(response_message.format(cases, deaths, date, time), quote=False)
 
 
 def sao_paulo(bot, update: Update):
@@ -67,19 +64,14 @@ def sao_paulo(bot, update: Update):
     update.message.reply_text(response_message.format(cases, suspects, deaths), quote=False)
 
 
-def start(bot, update: Update):
-    update.message.reply_text('OIII', quote=False)
-
-
 def webhook(request):
     bot = Bot(token=os.environ["TELEGRAM_TOKEN"])
     
     dispatcher = Dispatcher(bot, None, 0)
     
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('coronaMundo', world))
-    dispatcher.add_handler(CommandHandler('coronaBrasil', brazil))
-    dispatcher.add_handler(CommandHandler('coronaSP', sao_paulo))
+    dispatcher.add_handler(CommandHandler('coronamundo', world))
+    dispatcher.add_handler(CommandHandler('coronabrasil', brazil))
+    dispatcher.add_handler(CommandHandler('coronasp', sao_paulo))
 
     if request.method == 'POST':
         update = Update.de_json(request.get_json(force=True), bot)
