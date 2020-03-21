ccc5597c4e51aebbeb40982411ffb4c91a2444e0
Wallace Ramon Nogueira Soares <49095811+rheimon1@users.noreply.github.com>
2020-03-20 21:53:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,68 +1,46 @@
 import os
 import requests
+import json
 from telegram import Bot, Update
 from telegram.ext import Dispatcher, CommandHandler
 
 
 def brazil(bot, update: Update):
-    # Dados da requisição feita na API do coronaanalytic
-    dataBrazil = requests.get("https://api.coronaanalytic.com/brazil").json()
+    # Dados da requisição feita na API
+    req = requests.get("https://coronavirus-19-api.herokuapp.com/countries/Brazil")
 
-    values = dataBrazil["values"]
+    dataBrazil = req.content.decode()
 
-    deaths, cases, suspects = 0, 0, 0
-    date = str(dataBrazil["date"])
-    time = str(dataBrazil["time"])
+    dataBrazil = json.loads(dataBrazil)
 
-    for state in values:
-        for key, value in state.items():
-            if key == 'deaths':
-                deaths += value
-            if key == 'cases':
-                cases += value
-            if key == 'suspects':
-                suspects += value
+    cases = dataBrazil["cases"]
+    critical = dataBrazil["critical"]
+    todayCases = dataBrazil["todayCases"]
+    deaths = dataBrazil["deaths"]
+    recovered = dataBrazil["recovered"]
 
-    response_message = "Informações gerais sobre o corona vírus (COVID-19) no Brasil\n\n - Casos: {}; \n - Suspeitas: {}; \n - Mortes: {}. \n\n Última atualização: {} às {}. \n\n Todas as informações são atualizadas a cada hora com a fonte de dados fornecida pelo governo do Brasil: https://saude.gov.br"
+    response_message = "Informações gerais sobre o corona vírus (COVID-19) no Brasil\n\n - Casos: {}; \n - Casos críticos: {};\n - Casos novos: {};\n - Mortes: {};\n - Recuperados: {}. \n\n Fonte: https://www.worldometers.info/coronavirus/"
 
-    update.message.reply_text(response_message.format(cases, suspects, deaths, date, time), quote=False)
+    update.message.reply_text(response_message.format(cases, critical, todayCases, deaths, recovered), quote=False)
 
 
 def world(bot, update: Update):
-    req = requests.get("https://api.coronaanalytic.com/").json()
+    req = requests.get("https://coronavirus-19-api.herokuapp.com/all")
 
-    dataWorld = req["world"]
+    # convert from byte to string
+    dataWorld = req.content.decode()
 
-    deaths = 0
-    cases = 0
-    date = str(dataWorld["date"])
-    time = str(dataWorld["time"])
+    # convert json
+    dataWorld = json.loads(dataWorld)
 
-    valuesWorld = dataWorld["values"]
+    cases = dataWorld["cases"]
+    deaths = dataWorld["deaths"]
+    recovered = dataWorld["recovered"]
 
-    for country in valuesWorld:
-        for key, value in country.items():
-            if key == 'deaths':
-                deaths += value
-            if key == 'cases':
-                cases += value
+    response_message = "Informações gerais sobre o corona vírus (COVID-19) no Mundo\n\n - Casos: {};\n - Mortes: {};\n - Recuperados: {}. \n\n Fonte: https://www.worldometers.info/coronavirus/"
 
-    response_message = "Informações gerais sobre o corona vírus (COVID-19) no mundo\n\n - Casos: {} \n - Mortes: {}. \n\n Última atualização: {} às {}. \n\n Todas as informações são atualizadas a cada hora com a fonte de dados fornecida pelo governo do Brasil: https://saude.gov.br"
-
-    update.message.reply_text(response_message.format(cases, deaths, date, time), quote=False)
-
-
-def sao_paulo(bot, update: Update):
-    dataSaoPaulo = requests.get("https://api.coronaanalytic.com/brazil/35").json()
-
-    cases = dataSaoPaulo["cases"]
-    suspects = dataSaoPaulo["suspects"]
-    deaths = dataSaoPaulo["deaths"]
-
-    response_message = "Informações gerais sobre o corona vírus (COVID-19) em São Paulo\n\n - Casos: {}; \n - Suspeitas: {}; \n - Mortes: {}. \n\n Todas as informações são atualizadas a cada hora com a fonte de dados fornecida pelo governo do Brasil: https://saude.gov.br"
-
-    update.message.reply_text(response_message.format(cases, suspects, deaths), quote=False)
-
+    update.message.reply_text(response_message.format(cases, deaths, recovered), quote=False)
+    
 
 def webhook(request):
     bot = Bot(token=os.environ["TELEGRAM_TOKEN"])
@@ -71,7 +49,6 @@ def webhook(request):
     
     dispatcher.add_handler(CommandHandler('coronamundo', world))
     dispatcher.add_handler(CommandHandler('coronabrasil', brazil))
-    dispatcher.add_handler(CommandHandler('coronasp', sao_paulo))
 
     if request.method == 'POST':
         update = Update.de_json(request.get_json(force=True), bot)
